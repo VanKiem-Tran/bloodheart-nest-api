2e32b0d02a9bcadcd685236f5be50f5c6f69f950
Kiem Tran <kiem.tran@team.enouvo.com>
2023-04-26 20:37:21 +0700

    fix swagger

--- src/setup-swagger.ts
+++ src/setup-swagger.ts
@@ -59,6 +59,17 @@ Routes is following REST standard (Richardson level 3)
     swaggerOptions: {
       persistAuthorization: true,
     },
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   });
 
   console.info(
